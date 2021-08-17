af3b53ed31c4dac1ace66801813e9a88b736f4d1
Dulitha Daluwatta <dulitha.ruvin@gmail.com>
2021-08-17 18:32:44 +0530

    react js styles added with css modules

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import './App.css';
+import classes from './App.module.css';
 import Person from './Person/Person';
 
 class App extends Component {
@@ -47,16 +47,8 @@ class App extends Component {
   }
 
   render() {
-
-    const style = {
-      backgroundColor: 'white',
-      font: 'inherit',
-      border: '1px solid blue',
-      padding: '8px',
-      cursor: 'pointer'
-    }
-
     let persons = null;
+    let btnClass = '';
 
     if (this.state.showPersons) {
       persons = (
@@ -71,15 +63,22 @@ class App extends Component {
           })}
         </div>
       );
+      btnClass = classes.Red;
+    }
+
+    let asignedClasses = [];
+    if (this.state.persons.length <= 2) {
+      classes.push(classes.red);
+    }
+    if (this.state.persons.length <= 1) {
+      classes.push(classes.bold)
     }
 
     return (
-      <div className="App">
+      <div className={classes.App}>
         <h1>Hi, I'm a React App</h1>
-        <p>This is really working!</p>
-        <button
-          style={style}
-          onClick={this.togglePersonsHandler}>Toggle Persons</button>
+        <p className={asignedClasses.join(' ')}>This is really working!</p>
+        <button className={btnClass} onClick={this.togglePersonsHandler}>Toggle Persons</button>
         {persons}
       </div>
     );
